d169f2589cac7c14c3eb946ef226ada2d2c668ee
David Kossoglyad <david@logoui.co.il>
2020-03-10 15:23:54 +0200

    misc: dev app

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,12 +4,13 @@ import { Aurelia, PLATFORM } from 'aurelia-framework';
 import { WebpackLoader } from 'aurelia-loader-webpack';
 
 async function configure(aurelia: Aurelia): Promise<void> {
+  //console.log(PLATFORM.moduleName('node_modules/@aurelia-ej2-bridge/buttons/dist/es2015'));
   aurelia
     .use
     .developmentLogging()
     .standardConfiguration()
-    .plugin(PLATFORM.moduleName('@aurelia-ej2-bridge/buttons'))
-    
+    .plugin(PLATFORM.moduleName('@aurelia-ej2-bridge/base'))
+    //.plugin(PLATFORM.moduleName('@aurelia-ej2-bridge/buttons'))
     .plugin(PLATFORM.moduleName('aurelia-validation'));
 
   await aurelia.start();
